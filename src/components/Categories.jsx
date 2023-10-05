199e54166d07ea07d8dabf2eed957b31c47425d1
rel1se <botoevslava27032003@mail.ru>
2023-10-05 18:03:40 +0300

    1st Commit: Pizzas render

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Categories() {
+    const [category, setCategory] = React.useState(0)
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+    return (<div className="categories">
+        <ul>
+            {
+                categories.map((value, index) => (
+                    <li onClick={() => setCategory(index)} className={category === index ? "active" : ""}>{value}</li>
+                ))
+            }
+        </ul>
+    </div>)
+}
+
+export default Categories
